c68f07258e6e6310b492e2e7e0afdf7bb6695bb6
cruzmateo163 <62622906+cruzmateo163@users.noreply.github.com>
2022-10-27 14:24:28 -0500

    Updates

--- py_startup.py
+++ py_startup.py
@@ -1,13 +1,7 @@
-from load.load_channels import load_channels
-from transform.transform_channels import  tra_channels
+
+
 from util.db_connection import Db_Connection
-from extract.extract_channels import ext_channels
-from extract.extract_sales import ext_sales
-from extract.extract_promotions import ext_promotions
-from extract.extract_countries import ext_countries
-from extract.extract_products import ext_products
-from extract.extract_times import ext_times
-from extract.extract_customers import ext_customers
+
 from util.ETL_PROinse import ETLinsert
 import pandas as pd
 import traceback
